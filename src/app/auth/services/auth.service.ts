2beca280c6b193551acae75f2b9473e83ecccaef
Adrian Camarena <adriancamarena2310mail.com>
2023-12-03 17:47:10 -0600

    video-367 Servicio para verificar el Token de acceso

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,8 +1,9 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable, computed, inject, signal } from '@angular/core';
 import { Observable, catchError, map, of, tap, throwError } from 'rxjs';
 import { environment } from 'src/environments/environments';
 import { AuthStatus, LoginResponse, User } from '../interfaces';
+import { CheckTokenResponse } from '../interfaces/check-token.resonse';
 
 @Injectable({
   providedIn: 'root'
@@ -44,6 +45,36 @@ console.log(user, token);
 }
 
 
+checkAuthStatus():Observable<boolean> {
+
+  const url   = `${ this.baseUrl }/auth/check-token`;
+  const token = localStorage.getItem('token');
+
+  if ( !token ) {
+    return of(false);
+  }
+
+  const headers = new HttpHeaders()
+    .set('Authorization', `Bearer ${ token }`);
+
+
+    return this.http.get<CheckTokenResponse>(url, { headers })
+    .pipe(
+      map( ({ user, token }) =>  {
+        this._currentUser.set(user);
+        this._authStatus.set(AuthStatus.authenticated);
+        localStorage.setItem('token', token)
+
+        return true;
+      }),
+      catchError(() => {
+        this._authStatus.set( AuthStatus.notAuthenticated );
+        return of(false);
+      })
+    );
+
+
+}
 
 
 
