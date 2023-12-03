1dfeb0dddc2d2ef19fd26f0f75dbeb3308cd05bc
Adrian Camarena <adriancamarena2310mail.com>
2023-12-03 17:37:05 -0600

    video-366 Implementar lógica del Functional Guard

--- src/app/auth/guards/isAunteticated.guard.ts
+++ src/app/auth/guards/isAunteticated.guard.ts
@@ -1,10 +1,21 @@
-import type { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { Router, type CanActivateFn } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { AuthStatus } from '../interfaces';
 
 export const isAuthenticatedGuard: CanActivateFn = (route, state) => {
 
-console.log('Is authenticated');
-console.log([route, state]);
+// console.log('Is authenticated');
+// console.log([route, state]);
+const authService = inject( AuthService );
+const router = inject( Router );
 
+if ( authService.authStatus() === AuthStatus.authenticated ) {
+  return true;
+}
+
+router.navigateByUrl('/auth/login');
+return false;
 
 
   return true;
